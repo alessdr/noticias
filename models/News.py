263760a04ed932afd4b9dcbaf484aec24a576a16
Alessandro Rocha <alessdr@gmail.com>
2020-08-30 11:48:43 -0300

    MongoDB Access

--- models/News.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-
-class Post(Document):
-    title = StringField(required=True, max_length=200)
-    content = StringField(required=True)
-    author = StringField(required=True, max_length=50)
-    published = DateTimeField(default=datetime.datetime.now)
